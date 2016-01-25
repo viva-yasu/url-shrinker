fb028abcf45f433c62bd3369f757eec044362cbc
Yasunari Kondo <kyasunari123v@gmail.com>
2016-01-26 05:15:11 +0900

    テストの作成 #2

--- tests/VivaYasu/UriShrinker/UriShrinkerTest.php
+++ tests/VivaYasu/UriShrinker/UriShrinkerTest.php
@@ -2,7 +2,23 @@
 
 namespace VivaYasu\UriShrinker\Tests;
 
+use VivaYasu\UrlShrinker\UrlShrinker;
+
 class UriShrinkerTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  UrlShrinker $uri_shrinker */
+    private $uri_shrinker;
+    private $access_key = 'AIzaSyDk8JonB2nEr8nf4C0W4ztDlJDmlDQ-z0U';
+    private $long_url = 'https://github.com/viva-yasu/url-shrinker/';
 
+    public function setUp()
+    {
+        $this->uri_shrinker = new UrlShrinker($this->access_key, $this->long_url);
+    }
+
+    public function testGenerateShortUrl()
+    {
+        $result = $this->uri_shrinker->getShortUrl();
+        $this->assertEquals('https://goo.gl/o43GnB', $result);
+    }
 }
